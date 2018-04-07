f5dd6b70b021a4a0114a1a9b2a9deb84cd07581d
wssong85 <tabliser@gmail.com>
2018-04-07 17:33:45 +0900

    sss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { IndexPage } from '../pages/index/index';
 import { HashtagPage } from '../pages/hashtag/hashtag';
 import { MapSearchPage } from '../pages/map-search/map-search';
 
+import { GUserProvider } from '../providers/g-user/g-user';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -16,16 +18,16 @@ export class MyApp {
 	@ViewChild(Nav) nav: Nav;
 	
   	//rootPage:any = Detail;
-    rootPage:any = IndexPage;
-    //rootPage:any = Login;
+    //rootPage:any = IndexPage;
+    rootPage:any = Login;
     //rootPage:any = MapSearchPage;
-  
-  	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+  	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public gUser: GUserProvider) {
     	platform.ready().then(() => {
       		// Okay, so the platform is ready and our plugins are available.
       		// Here you can do any higher level native things you might need.
       		statusBar.styleDefault();
-      		splashScreen.hide();
+            splashScreen.hide();
     	});
 	}
 	
@@ -33,5 +35,3 @@ export class MyApp {
 		this.nav.push(HashtagPage);
 	}
 }
-
-
